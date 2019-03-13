728877b35d6fbcdefb3a8b5d5f3a5165e186618c
Patrice <Patrice@DESKTOP-AE1ABAV>
2019-03-13 15:36:26 +0100

    Commit and push dans un repo local = au rep projet

---
+++ src/main/java/com/urbanisation_si/previt/RechercherAssure/PersonneDao.java
@@ -0,0 +1,24 @@
+package com.urbanisation_si.previt.RechercherAssure;
+
+import java.util.List;
+
+/**
+ * 
+ * @author Patrice 1
+ *
+ */
+public interface PersonneDao {
+	 
+	public void ajouterPersonne(Personne e);
+	
+	public void modifierPersonne(Personne e);
+	
+	public void supprimerPersonne(Personne e);
+	
+	public Personne getById(int id);
+	
+	public List<Personne> getPersonnes();
+	
+	public List getPersonnebyNomEtPrenom(String pnom, String pprenom);
+		
+}
